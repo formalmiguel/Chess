ec2abcf71787764717248485262670b41ee373c8
formalmiguel <mig.alvarez92@gmail.com>
2025-10-31 17:09:04 -0500

    Chess.java now displays a GUI
    Main file now opens a JFrame to run the GUI portion of the file.
    
    So far Chess Piece can only make proper movements but game logic not fully set up.

--- Chess/src/main/java/chess/Chess.java
+++ Chess/src/main/java/chess/Chess.java
@@ -1,8 +1,22 @@
 package chess;
 
+import chess.board.Board;
+import javax.swing.*;
+import java.awt.*;
+
 public class Chess {
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Chess Game");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Board boardPanel = new Board();
+            frame.add(boardPanel);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            frame.setSize(new Dimension(1100, 800));
+        });
         Game game = new Game();
         game.play();
 
